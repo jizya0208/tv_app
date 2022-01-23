8c797877c65e6df78ceb90399b7167d278cec7e3
jizya0208 <moridyan606@gmail.com>
2022-01-23 22:35:42 +0900

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  return <div>Welcome to Next.js!</div>;s
 }
 
 export default MyApp
